3d0ccf1d8ff0b9e2f55ad878690d1aa7d513be56
Sushil Shakya <sushil.shakya@fusemachines.com>
2019-11-19 14:16:20 +0545

    Add hello world project

---
+++ Project-1/hello-world/src/Header.js
@@ -0,0 +1,13 @@
+import React ,{Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <p className="App-header">
+        <h2>Learn React Header</h2>
+      </p>
+  );
+  }
+}
+
+export default Header
---
+++ Project-1/hello-world/src/Intro.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+class Intro extends Component {
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+          Learn React intro
+        </p>
+      </div>
+      );
+  }
+}
+
+export default Intro
