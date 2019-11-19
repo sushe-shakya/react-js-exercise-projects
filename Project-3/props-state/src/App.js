2d265991c11f86caffc2d96da34a1c648ee0e3bf
Sushil Shakya <sushil.shakya@fusemachines.com>
2019-11-19 14:17:33 +0545

    Add props-state project

---
+++ Project-3/props-state/src/App.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (<div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit
+        <code>src/App.js</code>
+        and save to reload.
+      </p>
+      <h3>prop number is: {this.props.propNumber}</h3>
+      <h3>prop string is: {this.props.propString}</h3>
+      <h3>prop object is: {this.props.propObject.obj1}</h3>
+
+      <Parent/>
+    </div>);
+  }
+}
+
+App.propTypes = {
+  propObject: PropTypes.object,
+  propString: PropTypes.string,
+  propNumber: PropTypes.number
+};
+
+App.defaultProps = {
+  propNumber: 3,
+  propString: "This is prop string",
+  propObject: {
+    obj1: "I am obj 1",
+    obj2: "I am obj 2",
+    obj3: "I am obj 3"
+  }
+};
+
+class Parent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cars: ['s-BMW', 's-Ford', 's-Nano', 's-Audi']
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({cars: this.state.cars.reverse()});
+  }
+  render() {
+    return (<div>
+      <button onClick={this.handleClick}>
+        Just some info</button>
+      <Cars msg="cars are cool" model="34567" coolCars={this.state.cars}/>
+    </div>);
+  }
+}
+
+Parent.defaultProps = {
+  cars: ['BMW', 'Ford', 'Nano', 'Audi']
+};
+
+class Cars extends Component {
+  render() {
+    console.log(this.props);
+    return (<div>
+      <h3>
+        I am from cars component</h3>
+      <p>{this.props.msg}</p>
+      <p>{this.props.model}</p>
+      <div>{
+          this.props.coolCars.map((item, i) => {
+            return <p key={i}>{item}</p>;
+          })
+        }</div>
+
+    </div>);
+  }
+}
+export default App;
